6408fcacca571cfe2415f96ca1ef18c917193419
VarunGovindB <31276825+VarunGovindB@users.noreply.github.com>
2017-08-30 16:14:49 +0530

    Create Lab 1: Problem 6.cpp

---
+++ Lab 1: Problem 6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() 
+{
+int r;
+cin>>r;
+cout<<"Diameter:"<<2*r;
+cout<<"Area:"<<22*r*r/7;
+cout<<"Circumference:"<<2*22*r/7;
+return 0;
+}
